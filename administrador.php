19cfada1999b7ad40f477220be81a4d088610768
Ivan Acevedo <ivan_lamega_13@hotmail.com>
2018-07-14 17:04:37 -0300

    Ahora no se puede acceder a la pagina de administrador mediante link

--- administrador.php
+++ administrador.php
@@ -1,9 +1,32 @@
 <?php
+    include('funciones/sesiones.php');
+    include('funciones/conexionBD.php');
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-print "Bienvenido administrador";
+
+    IniciarSesion();
+
+?>
+
+<?php
+
+    setlocale(LC_ALL, "es_RA.UTF-8");
+
+    if (!isset($_SESSION['QR']) || !ExisteQR($_SESSION['QR']))
+    {
+        CerrarSesion();
+    }
+    //Guardamos temporalmente el qr para ser usado mas adelante
+    $qrleido = $_SESSION['QR'];
+    $idQr = $_SESSION['idQR'];
+    
+    unset($_SESSION['QR']);
+    unset($_SESSION['idQR']);
+    
+    print "Bienvenido administrador " . $qrleido . " " . $idQr;
+    
+    print "<form action='resultados.php' method='post'>";
+    print "<input id='codigo' type='hidden' name='QR' value='$qrleido'>";
+    print "<input id='codigo' type='hidden' name='idQR' value='$idQr'>";
+    print "<input type='submit' value='Ver Resultados'>";
+    print "</form>";
 ?>
