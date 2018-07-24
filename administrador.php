c2559bcdc2b9523d855937a8cf40e7c55c376f29
Ivan Acevedo <ivan_lamega_13@hotmail.com>
2018-07-24 16:52:19 -0300

    Agregados nuevos botones y nuevo estilo

--- administrador.php
+++ administrador.php
@@ -1,37 +1,73 @@
-<?php
-    include('funciones/sesiones.php');
-    include('funciones/conexionBD.php');
+<html>
+    <head>
+       <title>Administrador</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="CSS/admin.css">
+        
+        <?php
+            include('funciones/sesiones.php');
+            include('funciones/conexionBD.php');
 
 
-    IniciarSesion();
+            IniciarSesion();
 
-    setlocale(LC_ALL, "es_RA.UTF-8");
+            setlocale(LC_ALL, "es_RA.UTF-8");
 
-    if (!isset($_SESSION['QR']) || !ExisteQR($_SESSION['QR']))
-    {
-        CerrarSesion();
-    }
+            if (!isset($_SESSION['QR']) || !ExisteQR($_SESSION['QR']))
+            {
+                CerrarSesion();
+            }
+        ?>
+    </head>
     
-    //Guardamos temporalmente el qr para ser usado mas adelante
-    $qrleido = $_SESSION['QR'];
-    $idQr = $_SESSION['idQR'];
-    
-    unset($_SESSION['QR']);
-    unset($_SESSION['idQR']);
-    
-    print "Bienvenido administrador " . $qrleido . " " . $idQr;
-    
-    print "<form action='resultados.php' method='post'>";
-    print "<input id='codigo' type='hidden' name='QR' value='$qrleido'>";
-    print "<input id='codigo' type='hidden' name='idQR' value='$idQr'>";
-    print "<input id='codigo' type='hidden' name='habilitar' value=0>";
-    print "<input type='submit' value='Ver Resultados'>";
-    print "</form>";
-    
-    print "<form action='resultados.php' method='post'>";
-    print "<input id='codigo' type='hidden' name='QR' value='$qrleido'>";
-    print "<input id='codigo' type='hidden' name='idQR' value='$idQr'>";
-    print "<input id='codigo' type='hidden' name='habilitar' value=1>";
-    print "<input type='submit' value='Iniciar votacion'>";
-    print "</form>";
-?>
+    <body>
+        <div class="botones">
+            <?php
+
+                //Guardamos temporalmente el qr para ser usado mas adelante
+                $qrleido = $_SESSION['QR'];
+                $idQr = $_SESSION['idQR'];
+
+                unset($_SESSION['QR']);
+                unset($_SESSION['idQR']);
+
+                //print "Bienvenido administrador " . $qrleido . " " . $idQr;
+                
+                print "<form action='cargareleccion.php' method='post'>";
+                print "<input id='codigo' type='hidden' name='QR' value='$qrleido'>";
+                print "<input id='codigo' type='hidden' name='idQR' value='$idQr'>";
+                print "<div><input type='submit' value='Cargar Eleccion' class='btnff6'></div>";
+                print "</form>";
+                
+                print "<form action='cargarlista.php' method='post'>";
+                print "<input id='codigo' type='hidden' name='QR' value='$qrleido'>";
+                print "<input id='codigo' type='hidden' name='idQR' value='$idQr'>";
+                print "<div><input type='submit' value='Cargar Listas' class='btnff6'></div>";
+                print "</form>";
+                
+                print "<form action='QRgenerar.php' method='post'>";
+                print "<input id='codigo' type='hidden' name='QR' value='$qrleido'>";
+                print "<input id='codigo' type='hidden' name='idQR' value='$idQr'>";
+                print "<div><input type='submit' value='Generar QR' class='btnff6'></div>";
+                print "</form>";
+                
+                print "<form action='resultados.php' method='post'>";
+                print "<input id='codigo' type='hidden' name='QR' value='$qrleido'>";
+                print "<input id='codigo' type='hidden' name='idQR' value='$idQr'>";
+                print "<input id='codigo' type='hidden' name='habilitar' value=1>";
+                print "<div><input type='submit' value='Iniciar votacion' class='btnff6'></div>";
+                print "</form>";
+                
+                print "<form action='resultados.php' method='post'>";
+                print "<input id='codigo' type='hidden' name='QR' value='$qrleido'>";
+                print "<input id='codigo' type='hidden' name='idQR' value='$idQr'>";
+                print "<input id='codigo' type='hidden' name='habilitar' value=0>";
+                print "<div><input type='submit' value='Cierre y Resultado' class='btnff6'></div>";
+                print "</form>";
+
+                
+            ?>
+        </div>
+        <img src="Imagenes/logo.png" style="width: 150px; height: 150px;">
+    </body>
+</html>
