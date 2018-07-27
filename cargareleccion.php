34f07bef9ed3756991ecd6dd1a8e0d6958749374
Ivan Acevedo <ivan_lamega_13@hotmail.com>
2018-07-27 18:24:48 -0300

    Agregado logo y mensaje de error a la pagina de cargar eleccion

--- cargareleccion.php
+++ cargareleccion.php
@@ -1,8 +1,8 @@
 <html>
     <head>
-       <title>Generador de QR</title>
+       <title>Cargar elección</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="CSS/generarqr.css">
+        <link rel="stylesheet" href="CSS/admin.css">
     </head>
     
     <body>
@@ -26,28 +26,53 @@
             unset($_POST['QR']);
             unset($_POST['idQR']);
         ?>
+        <hr>
+            <img src="Imagenes/logo.png" alt="logo" style="width: 150px; height: 150px;">
+            <a>Cargar elección</a>
+        <hr>
+        <?php
+            $agregar = 0;
+            if(isset($_POST["nombre"]) && isset($_POST["fecha"]) && isset($_POST["agregar"]))
+            {
+                $agregar = $_POST["agregar"];
+                if($agregar)
+                {
+                    if(InsertarEleccion($_POST["nombre"], $_POST["fecha"]))
+                    {
+                        print "<div id='cuadro'>";
+                        print "<p id='alerta'>Elección cargada</p>";
+                        print "</div>";
+                    }
+                    else
+                    {
+                        print "<div id='cuadroR'>";
+                        print "<p id='alertaR'>Hubo un error al cargar la elección</p>";
+                        print "</div>";
+                    }
+                }
+            }
+            
+            unset($_POST["nombre"]);
+            unset($_POST["fecha"]);
+            unset($_POST["agregar"]);
+        ?>
         
         <div class="botones">
-            <div><a>Cargar una nueva elección</a></div>
-            <form action='cargareleccion.php' method='post'>
-                <?php
-                    //Con esto podemos colocar varias elecciones sin tener que ingresar el codigo nuevamente
-                    print "<input id='codigo' type='hidden' name='QR' value='$qrleido'>";
-                    print "<input id='codigo' type='hidden' name='idQR' value='$idQr'>";
-                ?>
-                <div><input type ="text" name="nombre" align="center" placeholder="Nombre de elección" required></div>
-                <div><input type ="date" name="fecha" placeholder="Fecha de elección"></div>
-                <div><input type="submit" value="Cargar elección" id="boton" class="btnff6"></div>
-            </form>
+            <div id="centrar">
+                <a>Cargar una nueva elección</a>
+                <form action='cargareleccion.php' method='post'>
+
+                    <?php
+                        //Con esto podemos colocar varias elecciones sin tener que ingresar el codigo nuevamente
+                        print "<input id='codigo' type='hidden' name='QR' value='$qrleido'>";
+                        print "<input id='codigo' type='hidden' name='idQR' value='$idQr'>";
+                    ?>
+                    <input id='codigo' type='hidden' name='agregar' value='1'>
+                    <input class="data" type ="text" name="nombre" align="center" placeholder="Nombre de elección" required>
+                    <input class="data" type ="date" name="fecha" placeholder="Fecha de elección">
+                    <input type="submit" value="Cargar elección" id="boton" class="btnff6">
+                </form>
+            </div>
         </div>
-        <?php
-            if(isset($_POST["nombre"]) && isset($_POST["fecha"]))
-            {
-                InsertarEleccion($_POST["nombre"], $_POST["fecha"]);
-                unset($_POST["nombre"]);
-                unset($_POST["fecha"]);
-            }
-        ?>
-        <img src="Imagenes/logo.png" style="width: 150px; height: 150px;">
     </body>
 </html>
